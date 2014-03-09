0558d2e311566e123372ba277de59f9b92831e4a
200439 <piotrekwisz@gmail.com>
2014-03-09 22:01:21 +0100

    dodany komentarz

--- kasownik.cpp
+++ kasownik.cpp
@@ -2,10 +2,14 @@
 #include <fstream>
 using namespace std;
 
+/*! \file Program czyszczacy plik z zapisanymi czasami wykonania algorytmu
+ *
+ */
+
 int main() {
 	cout<<"Kasujesz wyniki"<<endl;
 	cin.ignore();
-	ofstream kasuj("wynik", ios::trunc);
-	cout<<"Plik \"wynik\" oprozniony."<<endl;
+	ofstream kasuj("wynik.csv", ios::trunc);
+	cout<<"Plik \"wynik.csv\" oprozniony."<<endl;
 	return 0;
 }
